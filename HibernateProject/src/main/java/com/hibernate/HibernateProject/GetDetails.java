d3c249cba0570c90b25ee16e21d6cb6bdf8ef401
Shivpoojan Rajak <52307455+shiva-sudo@users.noreply.github.com>
2024-03-20 18:43:06 +0530

    get() and load() method

--- HibernateProject/src/main/java/com/hibernate/HibernateProject/GetDetails.java
+++ HibernateProject/src/main/java/com/hibernate/HibernateProject/GetDetails.java
@@ -17,8 +17,32 @@ public class GetDetails {
 //		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 		
 		Session session = factory.openSession();
-		Student student = (Student)session.get(Student.class, 102);
-		System.out.println(student);
+		
+		/* load() method first will create proxy.
+		 * when we use object then it fire the Query 
+		 * 
+		 * Note :-  In the below statement only one time query will run 
+		 * 			when use Object then run query
+		 */
+		
+		Student student = (Student)session.load(Student.class, 102);
+		System.out.println(student.getName()+ " : "+student.getCity());
+		
+		Student student1 = (Student)session.load(Student.class, 102);
+		System.out.println(student1.getName()+ " : "+student1.getCity());
+		
+		/* get() method first will check if data is present in session,
+		 * then return if not then hit query in database 
+		 * 
+		 * Note :-  In the below statement only one time query will run 
+		 * 			because data is already present in session 
+		 */
+		
+		Address adr = session.get(Address.class, 2);
+		System.out.println(adr.getStreet()+ " : "+adr.getCity());
+		Address adr1 = session.get(Address.class, 2);
+		System.out.println(adr.getStreet()+ " : "+adr1.getCity());
+
 		
 		session.close();
 		factory.close();
